da1473bde270be7b9c1878ca26e8bdd7c932edd9
Raphael <okolonnaemeka2017@gmail.com>
2022-10-18 13:53:26 -0400

    created the playRound function

--- javascript.js
+++ javascript.js
@@ -9,6 +9,46 @@ function getComputerChoice(){
             return 'SCISSORS'        
     }
 }
-console.log(getComputerChoice());
+let playerSelection = prompt("please Select your Choice", "ROCK");
+let computerSelection = getComputerChoice();
+let computerRound = 0;
+let playerRound = 0;
+let outPutMessage = "";
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection){
+        //outPutMessage = "tie";
+    }
+    else if(playerSelection === "ROCK" && computerSelection === "SCISSORS"){
+        playerRound++;
+        //outPutMessage = "Player wins, Rock is stronger than Scissors";
+    }
+    else if(playerSelection === "PAPER" && computerSelection === "ROCK"){
+        playerRound++;
+        //outPutMessage = "YOU win !!, Paper is stronger than Rock";
+    }
+    else if(playerSelection === "SCISSORS" && computerSelection === "PAPER"){
+        playerRound++;
+        //outPutMessage = "YOU Win !!, SCISSORS is Stronger than PAPER";
+    }
+   //computer check
+
+   else if(computerSelection === "ROCK" && playerSelection === "SCISSORS"){
+    computerRound++;
+    //outPutMessage = "Computer wins, Rock is stronger than Scissors";
+}
+else if(computerSelection === "PAPER" && playerSelection === "ROCK"){
+    computerRound++;
+    //outPutMessage = "computer wins, Paper is stronger than Rock";
+}
+else if(computerSelection === "SCISSORS" && playerSelection === "PAPER"){
+    computerRound++;
+    outPutMessage = "computer wins, SCISSORS is Stronger than PAPER";
+}
+
+    
+   // return outPutMessage;
+  }
 
   
+ 
