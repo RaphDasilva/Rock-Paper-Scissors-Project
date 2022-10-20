5d890d2b276628fffc440debb158a807793e0084
Raphael <okolonnaemeka2017@gmail.com>
2022-10-20 08:51:43 -0400

    Completed the game

--- javascript.js
+++ javascript.js
@@ -9,53 +9,67 @@ function getComputerChoice(){
             return 'SCISSORS'        
     }
 }
-let playerSelection = "";
-let computerSelection = getComputerChoice();
-let computerRound = 0;
-let playerRound = 0;
-let outPutMessage = "";
+
+let computerScore = 0;
+let playerScore = 0;
+
 
 function playRound(playerSelection, computerSelection) {
     
     
     if(playerSelection === "ROCK" && computerSelection === "SCISSORS"){
-        playerRound++;
-        return "Player wins, Rock is stronger than Scissors";
+        playerScore++;
+        return ` You wins, Rock is stronger than Scissors. Score: ${playerScore} to ${computerScore} `;
         
     }
     else if(playerSelection === "PAPER" && computerSelection === "ROCK"){
-        playerRound++;
-        return "YOU win !!, Paper is stronger than Rock";
+        playerScore++;
+        return `YOU win !!, Paper is stronger than Rock..Score: ${playerScore} to ${computerScore}`;
         
     }
     else if(playerSelection === "SCISSORS" && computerSelection === "PAPER"){
-        playerRound++;
-        return "YOU Win !!, SCISSORS is Stronger than PAPER";
+        playerScore++;
+        return `YOU Win !!, SCISSORS is Stronger than PAPER .Score: ${playerScore} to ${computerScore}`;
         
     }
    //computer check
 
    else if(computerSelection === "ROCK" && playerSelection === "SCISSORS"){
-    computerRound++;
-    return "Computer wins, Rock is stronger than Scissors";
+    computerScore++;
+    return Computer `wins, Rock is stronger than Scissors.Score: ${playerScore} to ${computerScore}`;
     
 }
 else if(computerSelection === "PAPER" && playerSelection === "ROCK"){
-    computerRound++;
-    return "computer wins, Paper is stronger than Rock";
+    computerScore++;
+    return `computer wins, Paper is stronger than Rock..Score: ${playerScore} to ${computerScore}`;
     
 }
 else if(computerSelection === "SCISSORS" && playerSelection === "PAPER"){
-    computerRound++;
-    return "computer wins, SCISSORS is Stronger than PAPER";
+    computerScore++;
+    return computer `wins, SCISSORS is Stronger than PAPER..Score: ${playerScore} to ${computerScore}`;
     
 }
-else{
-    return "That was a draw."
+else { 
+    return `That was a draw. Score: ${playerScore} to ${computerScore}`
 }
 
     }
 
-    
+    function game(){
+        for(let i = 0; i < 5; i++){
+            playerSelection = prompt("Make a choice: Scissors, Paper, Rock");
+            playerSelection = playerSelection.toUpperCase();
+            const computerSelection = getComputerChoice();
+            console.log(playRound(playerSelection, computerSelection));
+        }
+     if(playerScore > computerScore){
+        console.log("You win");
+       }
+       else if(computerScore > playerScore){
+        console.log("Computer win");
+       }   else console.log("You tied");
+    }
+
+    game();
 
  
